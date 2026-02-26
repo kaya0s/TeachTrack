656167ddfdab9d3b5d673af3ce3dbb27a5e20a0f
kaya0s <erwin.lanzaderas@gmail.com>
2026-02-26 18:33:25 +0800

    add admin router

--- server/app/api/v1/routers/__init__.py
+++ server/app/api/v1/routers/__init__.py
@@ -1,6 +1,7 @@
-from app.api.v1.routers import auth_router, users_router, classrooms_router, sessions_router
+from app.api.v1.routers import admin_router, auth_router, users_router, classrooms_router, sessions_router
 
 __all__ = [
+    "admin_router",
     "auth_router",
     "users_router",
     "classrooms_router",
