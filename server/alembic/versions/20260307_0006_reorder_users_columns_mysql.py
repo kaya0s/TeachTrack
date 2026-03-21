c8508257c43528d9787797ca0ea778a4f47dd2ce
kaya0s <erwin.lanzaderas@gmail.com>
2026-03-22 01:59:32 +0800

    Reorder users columns for MySQL optimization

--- server/alembic/versions/20260307_0006_reorder_users_columns_mysql.py
+++ server/alembic/versions/20260307_0006_reorder_users_columns_mysql.py
@@ -5,8 +5,15 @@ Revises: 20260307_0005
 Create Date: 2026-03-07 00:00:01
 """
 from alembic import op
+import sqlalchemy as sa
 
-from migration_helpers import initial_baseline_schema_present
+
+def initial_baseline_schema_present(connection: sa.engine.Connection) -> bool:
+    insp = sa.inspect(connection)
+    if not insp.has_table("colleges"):
+        return False
+    col_names = {c["name"] for c in insp.get_columns("colleges")}
+    return "acronym" in col_names
 
 
 revision = "20260307_0006"
