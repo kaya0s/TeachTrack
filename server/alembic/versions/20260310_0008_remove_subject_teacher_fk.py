617fbcb43ed5ecb97c99390c21e1330bce6e8faa
kaya0s <erwin.lanzaderas@gmail.com>
2026-03-22 01:59:53 +0800

    Remove subject-teacher foreign key constraint

--- server/alembic/versions/20260310_0008_remove_subject_teacher_fk.py
+++ server/alembic/versions/20260310_0008_remove_subject_teacher_fk.py
@@ -8,7 +8,13 @@ Create Date: 2026-03-10 00:10:00
 from alembic import op
 import sqlalchemy as sa
 
-from migration_helpers import initial_baseline_schema_present
+
+def initial_baseline_schema_present(connection: sa.engine.Connection) -> bool:
+    insp = sa.inspect(connection)
+    if not insp.has_table("colleges"):
+        return False
+    col_names = {c["name"] for c in insp.get_columns("colleges")}
+    return "acronym" in col_names
 
 
 revision = "20260310_0008"
